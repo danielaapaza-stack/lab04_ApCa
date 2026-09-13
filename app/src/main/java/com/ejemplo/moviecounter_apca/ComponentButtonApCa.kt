5bb7a142873601b3f6d116fc15d2debe82db8c6e
danielaapaza-stack <daniela.apaza@tecsup.edu.pe>
2026-09-13 06:49:30 -0500

    feat: Add Button component ApCa

---
+++ app/src/main/java/com/ejemplo/moviecounter_apca/ComponentButtonApCa.kt
@@ -0,0 +1,12 @@
+package com.ejemplo.moviecounter_apca
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ComponentButtonApCa() {
+    Button(onClick = { }) {
+        Text(text = "Boton ApCa")
+    }
+}
