1ef3e865cde08d33cb1fd1f2496335cad09d0699
danielaapaza-stack <daniela.apaza@tecsup.edu.pe>
2026-09-13 06:59:45 -0500

    feat: Add Switch component ApCa

---
+++ app/src/main/java/com/ejemplo/moviecounter_apca/ComponentSwitchApCa.kt
@@ -0,0 +1,12 @@
+package com.ejemplo.moviecounter_apca
+
+import androidx.compose.material3.Switch
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ComponentSwitchApCa() {
+    Switch(
+        checked = true,
+        onCheckedChange = { }
+    )
+}
